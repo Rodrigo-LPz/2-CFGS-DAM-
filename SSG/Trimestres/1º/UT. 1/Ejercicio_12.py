43f8c61dff75775d5b7d9e6a65a0889b9948ebff
Rodrigo <135568662+Rodrigo-LPz@users.noreply.github.com>
2025-09-23 13:47:51 +0100

    Update Ejercicio_12.py

--- SSG/Trimestres/1º/UT. 1/Ejercicio_12.py
+++ SSG/Trimestres/1º/UT. 1/Ejercicio_12.py
@@ -10,9 +10,10 @@ nombre = input("Bienvenido al programa 'Calculadora de Números pares', por favo
 # Solicitar el número correspondiente a la cadena del número n.
 numeroN = int(input(f"\nBienvenido a la aplicacion {nombre}, ahora introduzca el número de la cadena hasta el que desea que llegue el cálculo de la suma: "))
 
-# Solución con programación 1.
+# Inicializarla variable en 0, pues es el elemento neutro de la suma.
 sumatorio = 0
 
+# Crear un bucle donde se recorre 
 for i in range(1, numeroN + 1):
     factorial = 1
 
@@ -20,16 +21,6 @@ for i in range(1, numeroN + 1):
         factorial *= j
 
     sumatorio += (i * (i - 1)) / factorial
-
-# Solución con programación 2.
-    #suma = sum(range(1, numeroN + 1)) # La función range genera una secuencia de números desde 1 hasta numero (inclusive), y sum los suma. No crea una lista fija, con objetos en memeoria, sino que genera los números sobre la marcha, lo que es más eficiente en términos de memoria.
-
-
-# Solución matemática.
-    #suma = (numeroN * (numeroN + 1)) // 2 # Se utiliza "//" en vez de "/" ya que el uso de "/" devuelve un valor float [con decimales (28.0)] y en este caso no es necesario, ya que el resultado siempre será un número entero.
-    #suma = (numeroN * (numeroN - 1)) // 2
-
-
 
 # Muestreo del resultado final.
 print(f"\n\nEl resultado final del sumatorio es {sumatorio} -->Sumatorio: {sumatorio} = {numeroN * (numeroN - 1)} / {factorial}.")
